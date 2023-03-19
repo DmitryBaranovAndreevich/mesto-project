1ab57ca34a0add7c9effe71cfd4fae4e85fe78bd
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-19 13:15:57 +0300

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,8 @@ module.exports = {
       repo: 'https://github.com/DmitryBaranovAndreevich/mesto-project.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `npm run build && scp -Cr ./dist/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': '',
+      'pre-setup': '',
     },
   },
 };
