72883d85ec75636e236d26c78cfd26aa10672494
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-19 12:04:19 +0300

    add: ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/develop',
+} = process.env;
+
 module.exports = {
+
   deploy: {
     production: {
-      ref: 'origin/develop',
-      'pre-deploy': `npm i && npm run build && scp -Cr ./dist/* user-admin@51.250.99.58:/home/praktikum/mesto-frontend`,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      path: DEPLOY_PATH,
+      'pre-deploy': `npm i && npm run build && scp -Cr ./dist/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
     },
   },
 };
