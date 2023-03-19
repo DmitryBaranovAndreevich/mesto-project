c5474c9a3cf608352a4de65d79d581357e27f17b
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-19 11:57:51 +0300

    add: ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   deploy: {
     production: {
+      ref: 'origin/develop',
       'pre-deploy': `npm i && npm run build && scp -Cr ./dist/* user-admin@51.250.99.58:/home/praktikum/mesto-frontend`,
     },
   },
